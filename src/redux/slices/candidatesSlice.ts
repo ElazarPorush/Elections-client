4b48ad7febed88297eb439858a98359712434d0c
Elazar Porush <lzrprws@gmail.com>
2024-11-10 17:05:32 +0200

    store setup

---
+++ src/redux/slices/candidatesSlice.ts
@@ -0,0 +1,47 @@
+import { ActionReducerMapBuilder, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { candidatesState, DataStatus } from "../../types/redux";
+import { ICandidate } from "../../types/candidate";
+
+const initialState: candidatesState = {
+    error: null,
+    status: DataStatus.IDLE,
+    candidates: []
+}
+
+export const fetchCandidates = createAsyncThunk('candidates/list',
+    async (_, thunkApi) => {
+        try {
+            const res = await fetch("http://localhost:8200/api/candidates/")
+            if (!res.ok) {
+                thunkApi.rejectWithValue("Can't get the list, please try again")
+            }
+            const data = await res.json()
+            thunkApi.fulfillWithValue(data)
+        } catch (err) {
+            thunkApi.rejectWithValue(err)
+        }
+    }
+)
+
+const candidatesSlice = createSlice({
+    name: "candidates",
+    initialState,
+    reducers: {},
+    extraReducers: (builder: ActionReducerMapBuilder<candidatesState>) => {
+        builder.addCase(fetchCandidates.pending, (state) => {
+            state.status = DataStatus.LOADING
+            state.error = null
+            state.candidates = []
+        }).addCase(fetchCandidates.fulfilled, (state, action) => {
+            state.status = DataStatus.SUCCESS
+            state.error = null
+            state.candidates = action.payload as unknown as ICandidate[]
+        }).addCase(fetchCandidates.rejected, (state, action) => {
+            state.status = DataStatus.LOADING
+            state.error = action.error as string
+            state.candidates = null
+        })
+    }
+})
+
+export default candidatesSlice
