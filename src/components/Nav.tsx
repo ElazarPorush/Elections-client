64e132035845f513f9305c38a0a8c3b5b4b032f7
Elazar Porush <lzrprws@gmail.com>
2024-11-10 17:37:59 +0200

    root state and conditional for user or admin

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useAppSelector, RootState } from '../redux/store'
 
 export default function Nav() {
+  const user = useAppSelector((state: RootState) => state.user)
   return (
     <div className='nav'>
+      {user.user ? (<>
+        <NavLink to={'/votes'}>Votes</NavLink>
+        {user.user.isAdmin && (<NavLink to={'/statistics'}>Statistics</NavLink>)}
+        <button onClick={() => alert("Log out successfully")}>Logout</button>
+      </>
+      ) : (
+      <>
         <NavLink to={'/login'}>Login</NavLink>
         <NavLink to={'/register'}>Register</NavLink>
-        <NavLink to={'/votes'}>Votes</NavLink>
-        <NavLink to={'/statistics'}>Statistics</NavLink>
-        <button onClick={() => alert("Log out successfully")}>Logout</button>
+      </>
+      )
+      }
     </div>
   )
 }
